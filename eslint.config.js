dae51e89172f1f904ce630712bf38bd7972d6a40
mahbubreza <mahbubrezachoudhury@gmail.com>
2025-07-31 18:53:37 +0600

    31st July from office

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,9 @@
 import js from "@eslint/js";
+import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import globals from "globals";
 
 export default defineConfig([
-    {
-        files: ["**/*.{js,mjs,cjs,jsx}"],
-        plugins: { js },
-        extends: ["js/recommended"],
-        languageOptions: { globals: globals.browser },
-    },
-    {
-        extends: ["eslint:recommended", "plugin:prettier/recommended"],
-        plugins: ["prettier"],
-        rules: {
-            "prettier/prettier": "error",
-        },
-    },
-
-    pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
 ]);
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Job;
+
+
 Route::get('/', function () {
     return view('home');
 });
 
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function(){
+    return view('jobs', [
+        'jobs'=> Job::all()
+    ]);
 });
 
+Route::get('/jobs/{id}', function($id){
+    $job = Job::find($id);
+    return view('job', ['job'=> $job]);
+});
 
 Route::get('/contact', function (){
     return view('contact');
